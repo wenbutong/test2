ec0cdd048e640b28a199e8c2640c197d334e4eb5
wenbutong <158519015@qq.com>
2022-09-14 00:02:27 +0800

    第二次提交

--- 3.py
+++ 3.py
@@ -5,3 +5,11 @@
 - 💞️ I’m looking to collaborate on ...
 - 📫 How to reach me ...
 """
+
+# 获取所有常用字符包括字母和常用标点
+all_letters = string.ascii_letters + " .,;'"
+
+# 获取常用字符数量
+n_letters = len(all_letters)
+
+print("n_letter:", n_letters)
